61d35591d3ba31c463026d32570bb1f554e0119c
MadDogMcNamara <rjmcnama@mtu.edu>
2014-03-17 02:14:57 -0500

    made to actually listen on port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8124;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello asdasdfWorld\n');
-}).listen();
+  response.end('Hello 2348923478907823409237890234898234790\n');
+}).listen(port);
 
 console.log('Server running at http://127.0.0.1:' + port + '/');
