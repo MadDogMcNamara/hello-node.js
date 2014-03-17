b57710119147b92317ae3a50a0e0e266af70bd2a
MadDogMcNamara <rjmcnama@mtu.edu>
2014-03-17 01:33:18 -0500

    changed port

--- server/server.js
+++ server/server.js
@@ -5,4 +5,4 @@ http.createServer(function (request, response) {
   response.end('Hello asdasdfWorld\n');
 }).listen(5000);
 
-//console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://127.0.0.1:8124/');
