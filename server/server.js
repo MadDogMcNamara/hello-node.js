0a27a9312ffbe589fd30e8233f1e8fdfddf033c7
MadDogMcNamara <rjmcnama@mtu.edu>
2014-03-17 02:00:19 -0500

    revert branch

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var port = process.env.PORT || 8124;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello asdasdfWorld\n');
-}).listen(5000);
+}).listen();
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://127.0.0.1:' + port + '/');
